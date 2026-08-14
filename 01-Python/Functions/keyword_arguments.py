1e8bb830286bdb183208f8b683fcdfd57522c98b
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:25:33 +0530

    Create keyword_arguments.py

---
+++ 01-Python/Functions/keyword_arguments.py
@@ -0,0 +1,20 @@
+# Keyword Arguments
+
+def student_info(name, age, course):
+    print("Name:", name)
+    print("Age:", age)
+    print("Course:", course)
+
+
+student_info(
+    name="Yatharth",
+    age=20,
+    course="B.Tech IT"
+)
+
+
+## expected outcome
+
+Name: Yatharth
+Age: 20
+Course: B.Tech IT
