3dfbacd8d8c4e349b45f3047fd5fb5d760c9fc74
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:27:10 +0530

    Create recursion.py

---
+++ 01-Python/Functions/recursion.py
@@ -0,0 +1,17 @@
+# Recursion
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+
+    return n * factorial(n - 1)
+
+
+result = factorial(5)
+
+print("Factorial:", result)
+
+
+## expected outcome
+
+Factorial: 120
