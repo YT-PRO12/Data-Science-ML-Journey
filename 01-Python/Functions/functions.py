1e0d131eb94cda2fc32d39061d68b719e7264f30
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:22:02 +0530

    Create functions.py

---
+++ 01-Python/Functions/functions.py
@@ -0,0 +1,17 @@
+# Basic Python Functions
+
+def greet():
+    print("Hello, Yatharth!")
+
+
+def welcome():
+    print("Welcome to my Python journey!")
+
+
+greet()
+welcome()
+
+## expected output
+
+Hello, Yatharth!
+Welcome to my Python journey!
