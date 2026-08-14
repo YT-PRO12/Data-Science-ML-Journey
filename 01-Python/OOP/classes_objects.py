9b8e6f86d0ec0ecfaf248c4586e844181b6e92e7
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:28:55 +0530

    Create classes_objects.py

---
+++ 01-Python/OOP/classes_objects.py
@@ -0,0 +1,17 @@
+# Classes and Objects
+
+class Student:
+    def display(self):
+        print("This is a student.")
+
+
+student1 = Student()
+student2 = Student()
+
+student1.display()
+student2.display()
+
+##  expected outcome
+
+This is a student.
+This is a student.
