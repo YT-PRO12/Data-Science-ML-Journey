adbfe054736faa411adea0760ace1d5074db73e6
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:26:22 +0530

    Create lambda_functions.py

---
+++ 01-Python/Functions/lambda_functions.py
@@ -0,0 +1,12 @@
+# Lambda Functions
+
+square = lambda x: x * x
+
+add = lambda a, b: a + b
+
+print("Square:", square(5))
+print("Addition:", add(10, 20))
+
+## expected outcome
+Square: 25
+Addition: 30
