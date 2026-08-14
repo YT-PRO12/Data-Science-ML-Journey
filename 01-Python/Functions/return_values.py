d35b8cc6520e5d464aefc3636e579af20d2a6d8c
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:23:30 +0530

    Create return_values.py

---
+++ 01-Python/Functions/return_values.py
@@ -0,0 +1,19 @@
+# Return Values
+
+def add(a, b):
+    return a + b
+
+
+def multiply(a, b):
+    return a * b
+
+
+sum_result = add(10, 20)
+multiply_result = multiply(5, 4)
+
+print("Sum:", sum_result)
+print("Multiplication:", multiply_result)
+
+## expected output
+Sum: 30
+Multiplication: 20
