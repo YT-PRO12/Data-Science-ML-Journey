f5088083f9d72eb10e2c376408934eb79c4786c5
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:24:24 +0530

    Create default_arguments.py

---
+++ 01-Python/Functions/default_arguments.py
@@ -0,0 +1,19 @@
+# Default Arguments
+
+def greet(name="Yatharth"):
+    print("Hello", name)
+
+
+greet()
+greet("Rahul")
+
+## expected output
+
+# Default Arguments
+
+def greet(name="Yatharth"):
+    print("Hello", name)
+
+
+greet()
+greet("Rahul")
