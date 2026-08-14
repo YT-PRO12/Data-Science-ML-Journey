238ce98e3bdd491e88f83c4e82ce828d2f2d33da
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:15:35 +0530

    Create tuples.py

---
+++ 01-Python/Basics/tuples.py
@@ -0,0 +1,8 @@
+# Python Tuples
+
+numbers = (10, 20, 30, 40, 50)
+
+print("Tuple:", numbers)
+print("First element:", numbers[0])
+print("Last element:", numbers[-1])
+print("Length:", len(numbers))
