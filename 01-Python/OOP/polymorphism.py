4166aa7a87b65f697528d63d7c78963e817153d7
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:33:20 +0530

    Create polymorphism.py

---
+++ 01-Python/OOP/polymorphism.py
@@ -0,0 +1,22 @@
+# Polymorphism
+
+class Dog:
+
+    def sound(self):
+        print("Dog says: Woof")
+
+
+class Cat:
+
+    def sound(self):
+        print("Cat says: Meow")
+
+
+animals = [Dog(), Cat()]
+
+for animal in animals:
+    animal.sound()
+
+## expected outcome
+Dog says: Woof
+Cat says: Meow
