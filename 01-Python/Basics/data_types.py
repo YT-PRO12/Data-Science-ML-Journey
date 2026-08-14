e8f19bc941a8ed479a2dd729b4615436a2821912
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:13:11 +0530

    Create data_types.py

---
+++ 01-Python/Basics/data_types.py
@@ -0,0 +1,23 @@
+# Python Data Types
+
+name = "Yatharth"       # String
+age = 20                # Integer
+height = 1.70           # Float
+student = True          # Boolean
+
+numbers = [10, 20, 30]  # List
+coordinates = (10, 20)  # Tuple
+unique_numbers = {1, 2, 3}  # Set
+person = {
+    "name": "Yatharth",
+    "age": 20
+}                       # Dictionary
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(student))
+print(type(numbers))
+print(type(coordinates))
+print(type(unique_numbers))
+print(type(person))
