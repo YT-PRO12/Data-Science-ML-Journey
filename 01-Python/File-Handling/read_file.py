651dcfd5ca158d706fcb71987a01c8cef863167c
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:43:43 +0530

    Update read_file.py

--- 01-Python/File-Handling/read_file.py
+++ 01-Python/File-Handling/read_file.py
@@ -7,3 +7,10 @@ content = file.read()
 print(content)
 
 file.close()
+
+
+## Ny running the following command we will get the output there in sample.txt file saved in the same folder=
+                               python read_file.py
+##OUTPUT
+Hello, my name is Yatharth.
+I am learning Python for Data Science and Machine Learning.
