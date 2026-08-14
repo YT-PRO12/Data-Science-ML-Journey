407de977b7b9130b4748e0531092c1067f73deba
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:16:56 +0530

    Create dictionaries.py

---
+++ 01-Python/Basics/dictionaries.py
@@ -0,0 +1,19 @@
+# Python Dictionaries
+
+student = {
+    "name": "Yatharth",
+    "age": 20,
+    "course": "B.Tech IT",
+    "skills": ["Python", "Java", "Machine Learning"]
+}
+
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Course:", student["course"])
+print("Skills:", student["skills"])
+
+# Add a new key
+student["college"] = "JECRC"
+
+print("\nUpdated Dictionary:")
+print(student)
