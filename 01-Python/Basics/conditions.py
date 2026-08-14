9a21cfd3edd932d99259b80c35cc350437be31a1
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:14:03 +0530

    Create conditions.py

---
+++ 01-Python/Basics/conditions.py
@@ -0,0 +1,17 @@
+# Conditional Statements
+
+marks = 85
+
+if marks >= 90:
+    grade = "A+"
+elif marks >= 80:
+    grade = "A"
+elif marks >= 70:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+else:
+    grade = "F"
+
+print("Marks:", marks)
+print("Grade:", grade)
