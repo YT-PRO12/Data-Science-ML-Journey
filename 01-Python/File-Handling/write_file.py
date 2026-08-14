6706d6e312d355018e84d1b9be3b1499f54e1cf8
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:45:21 +0530

    Create write_file.py

---
+++ 01-Python/File-Handling/write_file.py
@@ -0,0 +1,12 @@
+# Writing to a File
+
+with open("output.txt", "w") as file:
+    file.write("Hello, Yatharth!\n")
+    file.write("I am learning Python File Handling.\n")
+
+print("Data written successfully.")
+
+
+## THE FOLLOWING CODE WILL AUTOMATICALLY A FILE NAMED output.txt CONTAINING =
+                   Hello, Yatharth!
+           I am learning Python File Handling.
