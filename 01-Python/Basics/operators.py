92556dbe0a84ab8caa36858f7ef78ba6450681f9
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:13:40 +0530

    Create operators.py

---
+++ 01-Python/Basics/operators.py
@@ -0,0 +1,22 @@
+# Python Operators
+
+a = 20
+b = 5
+
+# Arithmetic Operators
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Power:", a ** b)
+
+# Comparison Operators
+print("a > b:", a > b)
+print("a < b:", a < b)
+print("a == b:", a == b)
+
+# Logical Operators
+print("AND:", a > 10 and b < 10)
+print("OR:", a > 10 or b > 10)
+print("NOT:", not(a > b))
