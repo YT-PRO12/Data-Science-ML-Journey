27e52c34f936633deba48d130504a4b33ea2742b
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:07:25 +0530

    Create variables.py

---
+++ 01-Python/Basics/variables.py
@@ -0,0 +1,11 @@
+# Python Variables
+
+name = "Yatharth"
+age = 20
+height = 1.70
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
