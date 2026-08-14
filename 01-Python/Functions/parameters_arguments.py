85f694f265fb887a2bcc03d890af79129eb35a02
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:22:41 +0530

    Create parameters_arguments.py

---
+++ 01-Python/Functions/parameters_arguments.py
@@ -0,0 +1,17 @@
+# Parameters and Arguments
+
+def greet(name):
+    print("Hello", name)
+
+
+def add(a, b):
+    print("Sum:", a + b)
+
+
+greet("Yatharth")
+add(10, 20)
+
+## expected output
+
+Hello Yatharth
+Sum: 30
