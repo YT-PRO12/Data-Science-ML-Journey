15447d5e0ed1c86c62f092dcf76e10e5d284df4b
Yatharth Goyal <yatharthgoyal657@gmail.com>
2026-08-14 14:34:08 +0530

    Create abstraction.py

---
+++ 01-Python/OOP/abstraction.py
@@ -0,0 +1,34 @@
+# Abstraction
+
+from abc import ABC, abstractmethod
+
+
+class Vehicle(ABC):
+
+    @abstractmethod
+    def start(self):
+        pass
+
+
+class Car(Vehicle):
+
+    def start(self):
+        print("Car starts with a key.")
+
+
+class ElectricCar(Vehicle):
+
+    def start(self):
+        print("Electric car starts with a button.")
+
+
+car = Car()
+electric_car = ElectricCar()
+
+car.start()
+electric_car.start()
+
+## expected outcome
+
+Car starts with a key.
+Electric car starts with a button.
